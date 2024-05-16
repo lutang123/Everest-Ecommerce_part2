bizz84/flutter-firebase-masterclass

Update AdminProductUploadController

ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
@@ -1,13 +1,18 @@
+import 'package:ecommerce_app/src/features/products/data/products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:ecommerce_app/src/utils/notifier_mounted.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'admin_product_upload_controller.g.dart';
 
 @riverpod
-class AdminProductUploadController extends _$AdminProductUploadController {
+class AdminProductUploadController extends _$AdminProductUploadController
+    with NotifierMounted {
   @override
   FutureOr<void> build() {
+    ref.onDispose(setUnmounted);
     // no-op
   }
 
@@ -17,11 +22,17 @@ class AdminProductUploadController extends _$AdminProductUploadController {
       final downloadUrl = await ref
           .read(imageUploadRepositoryProvider)
           .uploadProductImageFromAsset(product.imageUrl, product.id);
-      // TODO: save downloadUrl to Firestore
-      state = const AsyncData(null);
-      // TODO: on success, go to the edit product page
+      await ref
+          .read(productsRepositoryProvider)
+          .createProduct(product.id, downloadUrl);
+      ref.read(goRouterProvider).goNamed(
+        AppRoute.adminEditProduct.name,
+        params: {'id': product.id},
+      );
     } catch (e, st) {
-      state = AsyncError(e, st);
+      if (mounted) {
+        state = AsyncError(e, st);
+      }
     }
   }
 }

lutang123/Everest-Ecommerce_part2

ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
@@ -1,13 +1,18 @@
+import 'package:ecommerce_app/src/features/products/data/products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
+import 'package:ecommerce_app/src/utils/notifier_mounted.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'admin_product_upload_controller.g.dart';
 
 @riverpod
-class AdminProductUploadController extends _$AdminProductUploadController {
+class AdminProductUploadController extends _$AdminProductUploadController
+    with NotifierMounted {
   @override
   FutureOr<void> build() {
+    ref.onDispose(setUnmounted);
     // no-op
   }
 
@@ -17,11 +22,17 @@ class AdminProductUploadController extends _$AdminProductUploadController {
       final downloadUrl = await ref
           .read(imageUploadRepositoryProvider)
           .uploadProductImageFromAsset(product.imageUrl, product.id);
-      // TODO: save downloadUrl to Firestore
-      state = const AsyncData(null);
-      // TODO: on success, go to the edit product page
+      await ref
+          .read(productsRepositoryProvider)
+          .createProduct(product.id, downloadUrl);
+      ref.read(goRouterProvider).goNamed(
+        AppRoute.adminEditProduct.name,
+        pathParameters: {'id': product.id},
+      );
     } catch (e, st) {
-      state = AsyncError(e, st);
+      if (mounted) {
+        state = AsyncError(e, st);
+      }
     }
   }
 }